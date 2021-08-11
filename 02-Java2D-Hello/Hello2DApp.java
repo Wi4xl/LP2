e0821d43de77591f1dbf011b603b6c678b19cd94
Wi4xl <88320172+Wi4xl@users.noreply.github.com>
2021-08-11 14:19:04 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -18,17 +18,58 @@ class Hello2DFrame extends JFrame {
             }
         );
         this.setTitle("Java2D - Hello World!");
-        this.setSize(350, 350);
+        this.setSize(640, 540);
         this.setVisible(true);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
+       
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
-        int w = getWidth();
-        int h = getHeight();
-        g2d.drawLine(0,0, w,h);
-        g2d.drawLine(0,h, w,0);
+        
+        int w = 640;
+        int h = 540;
+
+      
+        g2d.setFont(new Font("Arial",Font.BOLD,20));
+        g2d.setPaint(Color.BLACK);
+        
+        g2d.drawString("Bandeira da Inglaterra", 200, 70);
+        
+        g2d.setColor(Color.BLUE);
+        g2d.fillRect(0,100,640,440);
+        
+        g2d.setColor(Color.WHITE);
+        g2d.setStroke(new BasicStroke(70));
+        g2d.drawLine(0,112, w,h);
+        g2d.drawLine(0,h, w,112);
+        
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(20));
+        g2d.drawLine(0,112, w,h);
+        g2d.drawLine(0,h, w,112);
+        
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(20));
+        g2d.drawLine(0,112, w,h);
+        
+        
+        g2d.setColor(Color.WHITE);
+        g2d.setStroke(new BasicStroke(120));
+        g2d.drawLine(320,160, 320,h);
+        
+        
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0,246,640,120);
+        
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(85));
+        g2d.drawLine(0,305, 640,305);
+        
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(85));
+        g2d.drawLine(320,143, 320,h);
+        
+        
     }
 }
